f839a3f53b3944febb56ca0445a7a2a124ee3952
SP07 <SP07@SP07.local>
2016-06-09 19:32:03 -0300

    subo lo primero

---
+++ APIClient.swift
@@ -0,0 +1,44 @@
+//
+//  APIClient.swift
+//  ClaseNetworking
+//
+//  Created by Administrador on 18/5/16.
+//
+//
+
+import Alamofire
+import ObjectMapper
+
+class APIClient {
+    
+    static let sharedClient = APIClient()
+    
+    private let baseURL = "http://api.football-data.org/v1/soccerseasons/424/teams"
+    
+    private init() {
+        
+    }
+    
+    func footballOnCompletion(onCompletion: (team: [Team]?, error: NSError?) -> Void) {
+        
+        
+        Alamofire.request(.GET, self.baseURL).validate().responseJSON { (response: Response<AnyObject, NSError>) -> Void in
+            print(response)
+            switch response.result {
+                
+            case .Failure(let error):
+                onCompletion(team: nil, error: error)
+            case .Success(let value):
+                
+                if let team = Mapper<Team>().mapArray(value["teams"]) {
+                    
+                    onCompletion(team: team, error: nil)
+                }else {
+                    onCompletion(team: nil, error: NSError(domain: "MyApp", code: 9999, userInfo: [NSLocalizedDescriptionKey: "Fallo el mapeo"]))
+                }
+            }
+            
+        }
+    }
+    
+}
---
+++ Obligatorio3v1/Team.swift
@@ -0,0 +1,24 @@
+//
+//  Team.swift
+//  Obligatorio3v1
+//
+//  Created by SP07 on 9/6/16.
+//  Copyright © 2016 SP07. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Team: Mappable {
+    var name: String?
+    var URLflag: String?
+
+    required init?(_ map: Map) {
+    
+    }
+
+    func mapping (map: Map){
+        self.name <- map ["name"]
+        self.URLflag <- map ["crestUrl"]
+    }
+}
