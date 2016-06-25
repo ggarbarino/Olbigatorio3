d6b680ab7345b7e602a464654302cc19db9ae4d2
SP07 <SP07@SP07.local>
2016-06-24 21:19:17 -0300

    fondo

---
+++ Obligatorio3v1/ViewControllerHome.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerHome.swift
+//  Obligatorio3v1
+//
+//  Created by SP07 on 24/6/16.
+//  Copyright © 2016 SP07. All rights reserved.
+//
+
+import Foundation
+class ViewControllerHome{
+    
+    
+}
--- Podfile
+++ Podfile
@@ -13,5 +13,6 @@ target 'Obligatorio3v1' do
   pod 'Firebase'
   pod 'Firebase/Core'
   pod 'Firebase/AdMob'
+  pod 'DZNEmptyDataSet'
 end
 
